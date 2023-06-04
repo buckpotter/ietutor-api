94996e7eb584411d03a158fee28df40e351bd5a5
tunglam241 <20521521@gm.uit.edu.vn>
2023-06-04 19:34:06 +0700

    api for getuser

--- src/main/java/ie303/ietutorapi/controllers/UserController.java
+++ src/main/java/ie303/ietutorapi/controllers/UserController.java
@@ -1,20 +1,35 @@
 package ie303.ietutorapi.controllers;
 
 import ie303.ietutorapi.models.User;
+import ie303.ietutorapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
 public class UserController {
     @Autowired
+    UserRepository userRepository;
     // Get all instructors
     @GetMapping("/instructors")
     public List<User> getInstructors() {
         // Get all user that have the role of 1 (instructor) from MongoDB databas
         return null;
+    }
+    @GetMapping("/user/{email}")
+    public Optional<User> getUserByID(@PathVariable String email)
+    {
+        Optional<User> user= userRepository.findUsersByEmail(email);
+        return user;
+    }
+    @GetMapping("/userId/{id}")
+    public Optional<User> getUseruserID(@PathVariable String id)
+    {
+        Optional<User> user= userRepository.findUsersById(id);
+        return user;
     }
 
     // Get instructor by id
--- src/main/java/ie303/ietutorapi/repositories/UserRepository.java
+++ src/main/java/ie303/ietutorapi/repositories/UserRepository.java
@@ -4,9 +4,13 @@ import ie303.ietutorapi.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     // find a user by id and update the role to 1
 //    User findByIdAndRole(String id, int role);
+    Optional<User> findUsersByEmail(String email);
+    Optional<User> findUsersById(String id);
 }
